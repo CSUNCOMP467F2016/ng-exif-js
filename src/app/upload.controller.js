b4c048185a9109139c5500d5a7ab163f5bf615c6
Chris <cederstrom@acm.org>
2016-12-05 21:04:12 -0800

    EXIF service

--- src/app/upload.controller.js
+++ src/app/upload.controller.js
@@ -1,8 +1,20 @@
 
 angular.module('metadataApp')
-    .controller('UploadController', [ '$log', '$scope', 'fileReader',
-        function ($log, $scope, fileReader) {
+    .controller('UploadController', [ '$log', '$scope', 'fileReader', 'exifService', '$sce',
+        function ($log, $scope, fileReader, exifService, $sce) {
             $log.debug('UploadController', fileReader);
+
+
+            /** register service */
+            $scope.getData = function(event) {
+                exifService.getData(event.currentTarget, function() {
+                        debugger;
+                        $log.debug(exifService.pretty(event.currentTarget));
+                        $scope.imageMetadata = $sce.trustAsHtml(exifService.pretty(event.currentTarget).replace(/(?:\r\n|\r|\n)/g, '<br />'));
+                    }
+
+                );
+            }
 
             /**
              * getFile
